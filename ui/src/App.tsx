0818a2ee72973348071fe71f1651b4721f30a1f4
Steve Lemlek <stevelemlek@gmail.com>
2023-06-29 21:33:07 -0500

    loader image test

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Button from '@mui/material/Button';
 import { createDockerDesktopClient } from '@docker/extension-api-client';
 import { Stack, TextField, Typography } from '@mui/material';
+import Loader from './components/Loader/Loader'
 
 // Note: This line relies on Docker Desktop's presence as a host application.
 // If you're running this React app in a browser, it won't work properly.
@@ -30,6 +31,7 @@ export function App() {
         avoiding custom styling will help make sure your extension continues to
         look great as Docker's theme evolves.
       </Typography>
+      <Loader />
       <Typography variant="body1" color="text.secondary" sx={{ mt: 2 }}>
         Pressing the below button will trigger a request to the backend. Its
         response will appear in the textarea.
