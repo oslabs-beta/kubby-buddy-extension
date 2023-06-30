6d882f79bff2a9c90e1efe653bd9f761cb74aa34
Steve Lemlek <stevelemlek@gmail.com>
2023-06-29 22:28:00 -0500

    feat: main nav component added

---
+++ ui/src/components/Mainnav/MainNav.tsx
@@ -0,0 +1,18 @@
+// contain our containers
+
+import React, { FC, useContext } from 'react';
+import { DockerContainers } from '../Container/DockerContainer';
+import { Images } from '../Images/Images';
+import { UserContext } from '../../UserContext';
+// import Loader from '../Loader/Loader';
+//using a ternary based on booleans from useContext to switch the views
+
+export const MainNav: FC = () => {
+  const { showing } = useContext(UserContext);
+
+  return (
+    <div className="main-nav">
+      <ul>{showing === 'Containers' ? <DockerContainers /> : <Images />}</ul>
+    </div>
+  );
+};
